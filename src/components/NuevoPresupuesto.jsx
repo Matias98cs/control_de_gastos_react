220a04ebf78001c87ba72eea01d7d8108df00978
Matias98cs <matias98cs@gmail.com>
2022-11-12 12:49:33 -0300

    componentes header, mensaje y nuevopresupuesto

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje('No es un presupuesto valido')
+        }
+
+    }
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form className='formulario' onSubmit={handlePresupuesto}>
+            <div className='campo'>
+                <label htmlFor="">Definir Presupuesto</label>
+                <input 
+                    type="text" 
+                    className='nuevo-presupuesto'
+                    placeholder='Añade tu presupuesto'
+                    value={presupuesto}
+                    onChange={ e => setPresupuesto(e.target.value)} 
+                />
+            </div>
+            <input type="submit" value="Anadir" />
+
+            {mensaje && <Mensaje tipo="error" >{mensaje}</Mensaje> }
+            
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
