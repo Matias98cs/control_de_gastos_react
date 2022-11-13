0384acbf87474694dc8be620a9e39cdd7ba18cdb
Matias98cs <matias98cs@gmail.com>
2022-11-12 23:13:41 -0300

    importando componente modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import { useState } from 'react'
-import Header from './components/Header'
+import { useState } from "react";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false)
 
-  const [presupuesto, setPresupuesto] = useState(0)
-  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+  const handleNuevoGasto = () => {
+    setModal(true)
 
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 400)
+  }
 
   return (
     <div>
-      <Header 
+      <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      {isValidPresupuesto ? (
+        <div className="nuevo-gasto">
+          <img 
+            src={IconoNuevoGasto} 
+            alt="icono nuevo gasto" 
+            onClick={handleNuevoGasto}
+            />
+        </div>
+      ) : null}
+
+      {modal && <Modal setModal={setModal} animarModal={animarModal} setAnimarModal={setAnimarModal}/>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
